26fa86c2294a3d38d309d68cb0cf08036606adef
Guilherme Rainho Geraldo <143048354+guirainho@users.noreply.github.com>
2023-09-19 15:06:20 -0300

    Update ex10.c

--- ex10.c
+++ ex10.c
@@ -1,3 +1,15 @@
+Escreva uma função que recebe dois vetores inteiros A[] e B[] e seus tamanhos. A função deve imprimir
+a UNIÃO entre os vetores, ou seja, os elementos do vetor A mais aos elementos do vetor
+B, desconsiderando os números repetidos de A em B. Os vetores dados não possuem valores duplicados e
+não estão ordenados.
+Exemplo:
+A[] = { 7, 2, 5, 8, 4} e B[]= {4, 2, 9, 5} então 
+A  B= {7, 2, 5, 8, 4, 9}
+A[] = { 3, 9, 11} e B[]= {2, 6, 1} então 
+A  B = {3, 9, 11, 2, 6, 1}
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 #include <stdio.h>
 
 void imprimirUniao(int A[], int tamanhoA, int B[], int tamanhoB) {
